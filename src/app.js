1a332e06c97070c02b4bb8d65fe536233f109283
Pyter_dev <pyter_dev@pop-os.localdomain>
2021-04-16 14:58:11 -0300

    added support to static files

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 // server
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
 const routes = require('./routes.js')
 
+// static files
+app.use(express.static('public'))
+
+// routes
 app.get('/', routes.index)
 
 app.listen(port, () => {
