1e9276869e2649cc7b745d030678ca726d04f046
Pyter_dev <pyter_dev@pop-os.localdomain>
2021-04-16 19:55:36 -0300

    created all the functions for the delivery of the static pages

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 // server
 const express = require('express')
-const path = require('path')
 const app = express()
 const port = 3000
 
@@ -11,6 +10,10 @@ app.use(express.static('public'))
 
 // routes
 app.get('/', routes.index)
+app.get('/create-event', routes.pageCreateEvent)
+app.get('/event', routes.pageEvent)
+app.get('/events', routes.pageEvents)
+app.get('/sucess', routes.pageSucess)
 
 app.listen(port, () => {
   console.log(`Working in port ${port}`)
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,23 @@
 const routes = {
   index(req, res) {
     res.sendFile(__dirname + '/view/index.html')
-  }
+  },
+
+  pageCreateEvent(req, res) {
+    res.sendFile(__dirname + '/view/page-create-event.html')
+  },
+
+  pageEvent(req, res) {
+    res.sendFile(__dirname + '/view/page-event.html')
+  },
+
+  pageEvents(req, res) {
+    res.sendFile(__dirname + '/view/page-events.html')
+  },
+
+  pageSucess(req, res) {
+    res.sendFile(__dirname + '/view/page-sucess.html')
+  },
 }
 
 module.exports = routes
