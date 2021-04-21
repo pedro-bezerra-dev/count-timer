4a7e0d24a4fd6578244e2385166adf056ed616c8
Pyter_dev <pyter_dev@pop-os.localdomain>
2021-04-21 09:13:32 -0300

    added id field in fakedata.js

--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -1,9 +1,11 @@
 module.exports = [
   {
+    id: 1,
     name: 'Festa',
     date: ['2021','04','09','21','20']
   },
   {
+    id: 2,
     name: 'Jantar com a família',
     date: ['2021','05','12','20','00']
   }
