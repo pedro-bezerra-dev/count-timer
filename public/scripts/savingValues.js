1886f5319f817e9a76c3e7247d4722c15ad5387b
Pyter_dev <pyter_dev@pop-os.localdomain>
2021-04-23 21:42:31 -0300

    main function added

---
+++ public/scripts/savingValues.js
@@ -0,0 +1,16 @@
+function handlingBackendData() {
+  const allInputs = document.querySelectorAll('input[type="hidden"]')
+  
+  allInputs.forEach((input) => {
+    if(input.id.includes('event-name')) {
+      const eventName = input.value
+      eventsNames.push(eventName)
+    }
+    if(input.id.includes('target-date')) {
+      const eventDate = input.value
+      eventsDates.push(eventDate)
+    }
+  })
+}
+
+export { handlingBackendData }
