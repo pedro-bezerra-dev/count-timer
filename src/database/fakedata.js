415a7e9099d771fc8b22cf36d731bdf591279a15
Pyter_dev <pyter_dev@pop-os.localdomain>
2021-04-23 21:40:52 -0300

    file to save values from the created backend

--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -2,11 +2,11 @@ module.exports = [
   {
     id: 1,
     name: 'Festa',
-    date: '2021,03,23,20,44'
+    date: '2021,03,23,21,36'
   },
   {
     id: 2,
     name: 'Jantar com a família',
-    date: '2021,05,23,20,44'
+    date: '2021,03,23,21,37'
   }
 ]
