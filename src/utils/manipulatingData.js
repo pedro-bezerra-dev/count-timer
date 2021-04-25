764868ef21c7644576b4994b60119927f7eb1d6d
Pyter_dev <pyter_dev@pop-os.localdomain>
2021-04-25 20:47:04 -0300

    moving manipulatingData.js to utils directory

---
+++ src/utils/manipulatingData.js
@@ -0,0 +1,10 @@
+function dataConverter(eventData) {
+  const date = eventData.date
+  const hour = eventData.hour
+
+  const eventDate = date.replace(/-/g, ',') + ',' + hour.replace(':', ',')
+  
+  return eventDate
+}
+
+module.exports = { dataConverter }
