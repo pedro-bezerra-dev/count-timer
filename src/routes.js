49a0cacc8b72c1bb631996cad0b39950778e4e81
Pyter_dev <pyter_dev@pop-os.localdomain>
2021-04-26 13:33:14 -0300

    function for recording new events in the bank created

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,7 @@
 const fakedata = require('./database/fakedata.js')
+const dataConverter = require('./utils/manipulatingData.js')
+const database = require('./database/dbInit.js')
+const createEvent = require('./database/createEvent.js')
 
 const routes = {
   index(req, res) {
@@ -7,6 +10,28 @@ const routes = {
 
   pageCreateEvent(req, res) {
     res.render('page-create-event.html')
+  },
+
+  async createEvent(req, res) {
+    const eventData = req.body
+    const eventName = eventData.name
+    const eventDate = dataConverter(eventData)
+
+    try {
+
+      console.log(eventName, eventDate)
+
+      const db = await database
+      await createEvent(db, eventName, eventDate)
+
+    } catch (error) {
+
+      console.log(error)
+      res.send("Oops! It looks like an error has occurred. Please try restarting the app. If that doesn't work please could you report us on our page: https://github.com/pedro-henrique-sb/count-timer/issues")
+      
+    }
+
+    res.redirect('/sucess')
   },
 
   pageEvent(req, res) {
