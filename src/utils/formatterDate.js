b4c30d27ecd244d863231126a6edf3a491abb245
Pyter_dev <pyter_dev@pop-os.localdomain>
2021-04-30 11:13:01 -0300

    deleted formaterDate.js because it will no longer be used

--- src/utils/formatterDate.js
+++
@@ -1,9 +0,0 @@
-function formaterDate(date) {
-  date = date.substring(0, 10)
-  const data = date.split(',')
-  let formatedDate = `${data[2] + '/' + data[1] + '/' + data[0]}`
-
-  return formatedDate
-}
-
-module.exports = formaterDate
