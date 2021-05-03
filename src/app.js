b64bddfa01b1032cd94a9fdfed752f0510d02349
Pyter_dev <pyter_dev@pop-os.localdomain>
2021-05-03 15:17:49 -0300

    all the main.js content has been transformed into one that is exported

--- src/app.js
+++ src/app.js
@@ -1,31 +1,35 @@
-// server
-const express = require('express');
-const nunjucks = require('nunjucks');
+function startServer() {
+  // server
+  const express = require('express');
+  const nunjucks = require('nunjucks');
 
-const app = express();
-const port = 3000;
-const routes = require('./routes.js');
+  const app = express();
+  const port = 3000;
+  const routes = require('./routes.js');
 
-// configuring template engine
-nunjucks.configure('src/view', {
-  autoescape: true,
-  express: app,
-});
+  // configuring template engine
+  nunjucks.configure('src/view', {
+    autoescape: true,
+    express: app,
+  });
 
-// static files
-app.use(express.static('public'));
+  // static files
+  app.use(express.static('public'));
 
-// allowing encrypted requests
-app.use(express.urlencoded({ extended: true }));
+  // allowing encrypted requests
+  app.use(express.urlencoded({ extended: true }));
 
-// routes
-app.get('/', routes.index);
-app.get('/create-event', routes.pageCreateEvent);
-app.post('/create-event', routes.createEvent);
-app.get('/event', routes.pageEvent);
-app.get('/events', routes.pageEvents);
-app.get('/sucess', routes.pageSucess);
-app.get('/delete', routes.deleteEvent);
-app.get('/test', routes.pageTest);
+  // routes
+  app.get('/', routes.index);
+  app.get('/create-event', routes.pageCreateEvent);
+  app.post('/create-event', routes.createEvent);
+  app.get('/event', routes.pageEvent);
+  app.get('/events', routes.pageEvents);
+  app.get('/sucess', routes.pageSucess);
+  app.get('/delete', routes.deleteEvent);
+  app.get('/test', routes.pageTest);
 
-app.listen(port);
+  app.listen(port);
+}
+
+module.exports = startServer
