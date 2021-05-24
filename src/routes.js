dd5a05188edc44f8440132531ad9fbba49036be6
Pyter_dev <pyter_dev@pop-os.localdomain>
2021-05-24 12:41:09 -0300

    main function has been removed from routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ const path = require('path')
 const fakedata = require(path.join(__dirname, 'database', 'fakedata.js'));
 const dataConverter = require(path.join(__dirname, 'utils', 'manipulatingData.js'));
 const { addEvent, searchEvent , deleteEvent , getAllEvents } = require(path.join(__dirname, 'database', 'functions.js'));
-const { main: managerEvents } = require(path.join(__dirname, 'utils', 'managerEvents.js'))
 
 const routes = {
   index(req, res) {
