44d1d6c5e3011cbe57e1e9a9054786a4fed02656
Pyter_dev <pyter_dev@pop-os.localdomain>
2021-05-24 12:34:39 -0300

    the managerEvents.js file has been moved to the utils folder

---
+++ src/utils/managerEvents.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const { counter } = require(path.join(__dirname, '../', 'utils', 'counterHidden.js'))
+
+function main(allEvents) {
+  allEvents.forEach((event) => {
+    const eventName = event.name
+    const eventDate = event.date
+
+    counter(eventName, eventDate)
+  })
+}
+
+module.exports = { main }
