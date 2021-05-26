d35e3a0e8c21ac91f9dd18030c01f29b9183947a
Pyter_dev <pyter_dev@pop-os.localdomain>
2021-05-26 14:28:38 -0300

    features added to tray options

--- src/electron/tray.js
+++ src/electron/tray.js
@@ -17,11 +17,19 @@ function createWindow(route) {
 function main() {
   const tray = new Tray(path.join(__dirname, '../', '../', 'public', 'images', 'icons-png', 'logo.png'))
   const mainMenu = Menu.buildFromTemplate([
-    { label: 'Create event', type: 'normal'},
-    { label: 'Events', type: 'normal'},
+    { label: 'Create event', type: 'normal', click: () => {
+      createWindow('create-event')
+    }},
+    { label: 'Events', type: 'normal', click: () => {
+      createWindow('events')
+    }},
     { type: 'separator' },
-    { label: 'Open Count Timer', type: 'normal'},
-    { label: 'Shut down Count Timer', type: 'normal'}
+    { label: 'Open Count Timer', type: 'normal', click: () => {
+      createWindow('')
+    }},
+    { label: 'Shut down Count Timer', type: 'normal', click: () => {
+      app.quit()
+    }}
   ])
 
   tray.setToolTip('This is my application.')
