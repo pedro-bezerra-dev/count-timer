5b591f337a2016d03ca2131d7d28e5533707ab26
Pyter_dev <pyter_dev@pop-os.localdomain>
2021-05-28 17:13:16 -0300

    updateEvent function referenced in routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fakedata = require(path.join(__dirname, 'database', 'fakedata.js'));
 const { dataJoin, dataPart } = require(path.join(__dirname, 'utils', 'manipulatingData.js'));
-const { addEvent, searchEvent , deleteEvent , getAllEvents } = require(path.join(__dirname, 'database', 'functions.js'));
+const { addEvent, updateEvent, searchEvent , deleteEvent , getAllEvents } = require(path.join(__dirname, 'database', 'functions.js'));
 const { counter } = require(path.join(__dirname, 'utils', 'counterHidden.js'))
 
 const routes = {
